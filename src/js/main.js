538f0e65a9203462d3421990c90ef34f6bf3421a
DesperadoH <youxiyibai@126.com>
2016-11-19 14:33:00 +0800

    init

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		items: 1,
+		autoplay: true,
+		autoplayTimeout: 1000,
+		autoplayHoverPause: true
+	});
+
+})
+
