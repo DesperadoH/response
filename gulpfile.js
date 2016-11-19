4d669242a513abae536c2cfb991f6436caca9c68
DesperadoH <youxiyibai@126.com>
2016-11-19 14:57:58 +0800

    add dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
-var rev = require('gulp-rev');
-var revReplace = require('gulp-rev-replace');
-var useref = require('gukp-useref');
-var filter = require('gulp-filter');
-var uglify = require('gulp-uglify');
-var csso = require('gulp-csso');
+// gulp.com/plugins/
+var rev = require('gulp-rev'); // 添加版本号hash码把文件名修改掉
+var revReplace = require('gulp-rev-replace'); // 把index里的引用改成新的
+var useref = require('gukp-useref'); //在html里通过注释的方法写一些设置:
+// <!-- build:css css/combined.css -->
+// <link href="css/one.css" rel="stylesheet">
+// <link href="css/two.css" rel="stylesheet">
+// <!-- endbuild -->
+
+// <!-- build:js script/combined.js -->
+// <script type="text/javascript" src="script/one.js" ></script>
+// <script type="text/javascript" src="script/two.js" ></script>
+// <!-- endbuild -->
+var filter = require('gulp-filter');// 过滤器
+var uglify = require('gulp-uglify');// 压缩js
+var csso = require('gulp-csso'); // 压缩css
 
 gulp.task('default', function(){
 	var jsFilter = filter('**/*.js', {restore:true});
@@ -12,18 +22,18 @@ gulp.task('default', function(){
 	var indexHtmlFilter = filter(['**/*','!**/index.html'] ,{restore:true})
 
 	return gulp.src('src/index.html')
-	.pipe(useref())
-	.pipe(jsFilter)
-	.pipe(uglify())
-	.pipe(jsFilter.restore)
-	.pipe(cssFilter)
-	.pipe(csso())
-	.pipe(cssFilter.restore)
-	.pipe(indexHtmlFilter)
-	.pipe(rev())
-	.pipe(indexHtmlFilter.restore)
-	.pipe(revReplace());
-	.pipe(gulp.dest('dist'));
+		.pipe(useref())
+		.pipe(jsFilter)
+		.pipe(uglify())
+		.pipe(jsFilter.restore)
+		.pipe(cssFilter)
+		.pipe(csso())
+		.pipe(cssFilter.restore)
+		.pipe(indexHtmlFilter)
+		.pipe(rev())
+		.pipe(indexHtmlFilter.restore)
+		.pipe(revReplace());
+		.pipe(gulp.dest('dist'));
 
 })
 
